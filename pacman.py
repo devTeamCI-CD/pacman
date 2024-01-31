57a671f0a9d90699985781441031305a700d9d06
Arsen Mehdalskyi <arsen25public@gmail.com>
2024-01-31 22:59:58 +0200

    fixed checkstyle

--- pacman.py
+++ pacman.py
@@ -8,6 +8,4 @@ FPS = 60
 
 screen = pygame.display.set_mode([WINDOW_WIDTH, WINDOW_HEIGHT])
 timer = pygame.time.Clock()
-font = pygame.font.Font('freesansbold.ttf', 20)
-
-
+font = pygame.font.Font("freesansbold.ttf", 20)
