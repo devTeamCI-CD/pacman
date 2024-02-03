bf8b3f26c666899a616b7948506fcaadd572bb98
Nadin <nada2205nada@gmail.com>
2024-02-04 01:20:10 +0200

    set up food for pac-man

--- pacman.py
+++ pacman.py
@@ -35,6 +35,39 @@ flicker = False
 turns_allowed = [False, False, False, False]
 direction_command = 0
 player_speed = 2
+score = 0
+powerup = False
+power_counter = 0
+eaten_ghost = [False, False, False, False]
+moving = False
+ghost_speeds = [2, 2, 2, 2]
+startup_counter = 0
+lives = 3
+game_over = False
+game_won = False
+
+def draw_misc():
+    score_text = font.render(f'Score: {score}', True, 'white')
+    screen.blit(score_text, (10, 920))
+    if powerup:
+        pygame.draw.circle(screen, 'blue', (140, 930), 15)
+    for i in range(lives):
+        screen.blit(pygame.transform.scale(player_images[0], (30, 30)), (650 + i * 40, 915))
+
+def check_collisions(scor, power, power_count, eaten_ghosts):
+    num1 = (WINDOW_HEIGHT - 50) // 32
+    num2 = WINDOW_WIDTH // 30
+    if 0 < player_x < 870:
+        if level[center_y // num1][center_x // num2] == 1:
+            level[center_y // num1][center_x // num2] = 0
+            scor += 10
+        if level[center_y // num1][center_x // num2] == 2:
+            level[center_y // num1][center_x // num2] = 0
+            scor += 50
+            power = True
+            power_count = 0
+            eaten_ghosts = [False, False, False, False]
+    return scor, power, power_count, eaten_ghosts
 
 def draw_board():
     num1 = ((WINDOW_HEIGHT - 50) // 32)
@@ -148,13 +181,27 @@ while run:
     else:
         counter = 0
         flicker = True
+    if powerup and power_counter < 600:
+        power_counter += 1
+    elif powerup and power_counter >= 600:
+        power_counter = 0
+        powerup = False
+        eaten_ghost = [False, False, False, False]
+    if startup_counter < 180 and not game_over and not game_won:
+        moving = False
+        startup_counter += 1
+    else:
+        moving = True
 
     screen.fill('black')
     draw_board()
     draw_player()
+    draw_misc
     center_x = player_x + 23
     center_y = player_y + 24
     turns_allowed = check_position(center_x, center_y)
+    player_x, player_y = move_player(player_x, player_y)
+    score, powerup, power_counter, eaten_ghost = check_collisions(score, powerup, power_counter, eaten_ghost)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
